6ce9ebb146f26a01d27029d92c4cf4ffce0b123e
vsmithbe-git <vsmithbe.git@gmail.com>
2022-04-06 02:23:14 -0500

    new project

--- src/composable/useFirebase.js
+++ src/composable/useFirebase.js
@@ -1,7 +1,18 @@
 import { initializeApp } from "firebase/app";
 import {getAuth} from "firebase/auth";
-
+//Added
+import { getFirestore, collection, getDocs } from 'firebase/firestore/lite';
 import {firebaseConfig} from "../firebaseConfig";
 
 export const firebaseApp = initializeApp(firebaseConfig);
 export const firebaseAuth = getAuth(firebaseApp);
+// Added
+export const firebaseDB = getFirestore(firebaseApp);
+
+// Get a list of users from your database
+async function getUsers(firebaseDB) {
+    const usersCol = collection(firebaseDB, 'users');
+    const usersSnapshot = await getDocs(usersCol);
+    const usersList = usersSnapshot.docs.map(doc => doc.data());
+    return usersList;
+  }
